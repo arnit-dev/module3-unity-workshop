ab5b90649ab7d4b28cfcfcf884a1527e4cafeff9
Arnit Dev <arnit.dev@gmail.com>
2025-05-21 05:02:47 +0700

    Add Unit workshop module 3 to repository

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public InventoryPanel inventoryPanel;
+
+    public void OpenInventoryPanel()
+    {
+        inventoryPanel.gameObject.SetActive(true);
+        inventoryPanel.OnOpen();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+    }
+
+    public void CloseInventoryPanel()
+    {
+        inventoryPanel.gameObject.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+    }
+
+    public float timeCounter = 30f;
+    public ItemData targetItem;
+    public int targetAmout = 5;
+
+    public TMP_Text timeCounterText;
+    public Image targetItemIcon;
+    public TMP_Text targetCurrentAmountText;
+
+    public bool isPlayerWin = false;
+
+    private void Start()
+    {
+        targetItemIcon.sprite = targetItem.itemIcon;
+    }
+
+    private void Update()
+    {
+        if (isPlayerWin)
+            return;
+
+        if (timeCounter > 0f)
+        {
+            timeCounter -= Time.deltaTime;
+            timeCounterText.text = timeCounter.ToString();
+            targetCurrentAmountText.text = "x " + (targetAmout - InventoryManager.instance.GetItemAmount(targetItem)).ToString();
+
+            if (InventoryManager.instance.GetItemAmount(targetItem) >= targetAmout)//player win
+            {
+                Debug.Log("Player Win");
+                isPlayerWin = true;
+            }
+        }
+        else // player lose
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
